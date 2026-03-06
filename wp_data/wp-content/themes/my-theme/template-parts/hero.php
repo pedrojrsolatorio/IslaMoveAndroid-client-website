7a9b44940966d65df75cbb432e92efe0c015a8d3
Pedro Jr Solatorio <solatoriop@gmail.com>
2026-03-06 23:34:44 +0800

    use ACF and add fields for hero section

--- wp_data/wp-content/themes/my-theme/template-parts/hero.php
+++ wp_data/wp-content/themes/my-theme/template-parts/hero.php
@@ -1,13 +1,22 @@
+<?php
+$hero_title = get_field('hero_title');
+$hero_subtitle = get_field('hero_subtitle');
+$hero_button_text = get_field('hero_button_text');
+$hero_button_link = get_field('hero_button_link');
+?>
+
 <section class="bg-linear-to-r from-primary to-secondary text-white py-24">
     <div class="max-w-7xl mx-auto px-6 text-center">
         <h2 class="text-5xl font-bold mb-6">
-            Reliable Fixed-Fare Rides in San Jose Municipality
+            <?php echo esc_html($hero_title); ?>
         </h2>
         <p class="text-xl mb-8">
-            Municipality-approved pricing. Nearest highest-rated drivers. Safe. Transparent. Community-first.
+            <?php echo esc_html($hero_subtitle) ?>
         </p>
-        <a href="#download" class="bg-white text-primary px-8 py-3 rounded-lg font-semibold">
-            Get IslaMove on Android
-        </a>
+        <?php if ($hero_button_link): ?>
+            <a href="<?php echo esc_url($hero_button_link) ?>" class="bg-white text-primary px-8 py-3 rounded-lg font-semibold">
+                <?php echo esc_html($hero_button_text) ?>
+            </a>
+        <?php endif; ?>
     </div>
 </section>
