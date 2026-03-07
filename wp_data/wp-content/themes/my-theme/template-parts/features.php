3f5e825a09082c128c313d01aca5b2d924e89b79
Pedro Jr Solatorio <solatoriop@gmail.com>
2026-03-07 13:12:02 +0800

    add acf fields for features section

--- wp_data/wp-content/themes/my-theme/template-parts/features.php
+++ wp_data/wp-content/themes/my-theme/template-parts/features.php
@@ -1,16 +1,23 @@
+<?php
+$feat_title_1 = get_field('feature_title_1');
+$feat_description_1 = get_field('feature_description_1');
+$feat_title_2 = get_field('feature_title_2');
+$feat_description_2 = get_field('feature_description_2');
+?>
+
 <section class="py-20 bg-white">
     <div class="max-w-6xl mx-auto px-6 text-center">
-        <h3 class="text-3xl font-bold mb-12">Core Features</h3>
+        <h3 class="text-3xl font-bold mb-12"><?php echo esc_html($feat_title_1) ?></h3>
 
         <div class="grid md:grid-cols-2 gap-8">
             <div class="p-6 shadow rounded-xl">
-                <h4 class="font-semibold text-xl mb-2">Companion Booking</h4>
-                <p>Add companions and fare updates automatically.</p>
+                <h4 class="font-semibold text-xl mb-2"><?php  ?></h4>
+                <p><?php echo esc_html($feat_description_1) ?></p>
             </div>
 
             <div class="p-6 shadow rounded-xl">
-                <h4 class="font-semibold text-xl mb-2">Two-Way Ratings</h4>
-                <p>Drivers and passengers rate each other after every ride.</p>
+                <h4 class="font-semibold text-xl mb-2"><?php echo esc_html($feat_title_2) ?></h4>
+                <p><?php echo esc_html($feat_description_2) ?></p>
             </div>
         </div>
     </div>
