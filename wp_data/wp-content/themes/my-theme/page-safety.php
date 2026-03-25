e94d5abba1d0bc09043cb734d41f8a7dd7dc222e
Pedro Jr Solatorio <solatoriop@gmail.com>
2026-03-25 23:31:51 +0800

    add first section

--- wp_data/wp-content/themes/my-theme/page-safety.php
+++ wp_data/wp-content/themes/my-theme/page-safety.php
@@ -4,45 +4,27 @@ Template Name: Safety
 */
 get_header() ?>
 
-<main>
-    <section class="bg-white pt-20 md:pt-26 lg:pt-32 min-h-[calc(100vh-76px)]">
-        <div class="gap-6">
-            <span class="inline-block bg-blue-100 text-primary text-sm px-4 py-2 rounded-full mb-6">
-                Now Live in San Jose Municipality
-            </span>
+<main class="">
+    <section class="bg-slate-50 pt-20 md:pt-26 lg:pt-32 min-h-[calc(100vh-76px)]">
+        <div class="gap-6 wrapper flex flex-col items-center">
+            <div class="bg-blue-100 flex items-center gap-2 px-4 py-2 rounded-full">
+                <img src="<?php echo get_template_directory_uri() ?>/assets/icons/safety.png" alt="safety icon" class="w-3 h-3.5">
+                <span class="text-primary text-sm">
+                    YOUR SAFETY IS OUR PRIORITY
+                </span>
+            </div>
             <h1 class="text-6xl font-black leading-tight mb-6">
                 <?php echo wp_kses_post($hero_title); ?>
-                <!-- If for client who don't know how to use span in ACF field then used this code below and have separate ACF fields: -->
-                <!-- <?php if ($hero_title_plain): ?>
-                    <?php echo esc_html($hero_title_plain); ?>
-                <?php endif; ?>
-
-                <?php if ($hero_title_highlight): ?>
-                    <span class="text-primary"><?php echo esc_html($hero_title_highlight); ?></span>
-                <?php endif; ?>
-
-                <?php if ($hero_title_suffix): ?>
-                    <?php echo esc_html($hero_title_suffix); ?>
-                <?php endif; ?> -->
+                Building a <span class="text-primary">Trusted</span> Community
             </h1>
             <p class="text-gray-600 text-xl mb-8 max-w-lg">
-                <?php echo esc_html($hero_subtitle) ?>
+                IslaMove is more than just a ride. We've built a rigorous verification system to ensure every driver and passenger is vetted for your peace of mind.
             </p>
             <div class="flex mb-8">
-                <a href="<?php echo esc_url($hero_button_link) ?>" class="bg-black text-white rounded-full flex justify-center w-40 h-14 overflow-hidden px-6 py-2 hover:-translate-y-1 transition duration-300 ease-in-out">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/itch_io.png" alt="itch.io" class="max-w-full max-h-full object-contain">
+                <a href="<?php echo esc_url($hero_button_link) ?>" class="bg-primary rounded-full flex justify-center items-center h-14 gap-2 overflow-hidden px-6 py-2 hover:-translate-y-1 transition duration-300 ease-in-out">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/warning.png" alt="warning" class="w-4">
+                    Report a Concern
                 </a>
-            </div>
-            <div class="flex items-center mt-4">
-                <!-- Avatar Stack -->
-                <div class="flex -space-x-3">
-                    <img src="<?php echo get_template_directory_uri() ?>/assets/icons/avatar_pic.jpg" alt="User 1" class="w-8 h-8 rounded-full border-2 border-white">
-                    <img src="<?php echo get_template_directory_uri() ?>/assets/icons/avatar_pic2.jpg" alt="User 2" class="w-8 h-8 rounded-full border-2 border-white">
-                    <img src="<?php echo get_template_directory_uri() ?>/assets/icons/avatar_pic3.jpg" alt="User 3" class="w-8 h-8 rounded-full border-2 border-white">
-                </div>
-                <p class="text-gray-500 text-sm ml-4">
-                    Joined by <strong>100+ local residents</strong>
-                </p>
             </div>
         </div>
     </section>
