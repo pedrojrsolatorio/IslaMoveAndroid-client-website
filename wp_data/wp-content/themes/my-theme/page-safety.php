6c3b263b6a648cc64f95ffe87c907674a4ed6a8e
Pedro Jr Solatorio <solatoriop@gmail.com>
2026-03-30 23:06:03 +0800

    add cta section

--- wp_data/wp-content/themes/my-theme/page-safety.php
+++ wp_data/wp-content/themes/my-theme/page-safety.php
@@ -90,7 +90,7 @@ get_header() ?>
                 <h2>Two-Way Accountability</h2>
                 <p>Trust is a two-way street. Our mutual rating system ensures that respectful behavior is the standard for everyone in the IslaMove community.</p>
                 <div class="flex gap-4">
-                    <div class="bg-white shadow-md rounded-full flex items-center justify-center w-10 h-10">
+                    <div class="bg-white shadow-md rounded-full flex items-center justify-center w-10 h-10 shrink-0">
                         <svg class="w-5 h-4.75 text-primary" fill="currentColor" viewBox="0 0 20 20">
                             <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.286 3.957a1 1 0 00.95.69h4.162c.969 0 1.371 1.24.588 1.81l-3.367 2.447a1 1 0 00-.364 1.118l1.287 3.957c.3.921-.755 1.688-1.54 1.118l-3.367-2.447a1 1 0 00-1.176 0l-3.367 2.447c-.784.57-1.838-.197-1.539-1.118l1.287-3.957a1 1 0 00-.364-1.118L2.075 9.384c-.783-.57-.38-1.81.588-1.81h4.162a1 1 0 00.95-.69l1.274-3.957z" />
                         </svg>
@@ -101,7 +101,7 @@ get_header() ?>
                     </div>
                 </div>
                 <div class="flex gap-4">
-                    <div class="bg-white shadow-md rounded-full flex items-center justify-center w-10 h-10">
+                    <div class="bg-white shadow-md rounded-full flex items-center justify-center w-10 h-10 shrink-0">
                         <svg class="w-5 h-4.5 text-primary" fill="currentColor" viewBox="0 0 20 20">
                             <path fill-rule="evenodd" d="M18 10A8 8 0 114.293 4.293A8 8 0 0118 10zm-8-4a1 1 0 00-1 1v4a1 1 0 002 0V7a1 1 0 00-1-1zm0 8a1 1 0 100 2 1 1 0 000-2z" clip-rule="evenodd" />
                         </svg>
@@ -161,6 +161,56 @@ get_header() ?>
                     <p class="text-sm text-[#94A3B8]">"Professional driver, very clean car!"</p>
                 </div>
                 <button class="bg-secondary rounded-3xl py-3 text-white cursor-pointer transition-all duration-300 ease-in-out hover:-translate-y-1 hover:shadow-xl active:translate-y-0 active:shadow-md">Submit Rating</button>
+            </div>
+        </div>
+    </section>
+
+    <section class="wrapper flex flex-col py-24 gap-6 max-w-4xl mx-auto items-center text-center">
+        <h2>Our Community-First Approach</h2>
+        <p>IslaMove is built on respect, inclusivity, and safety. We remain a zero-tolerance policy for harasement or discrimination of any kind.</p>
+        <div class="grid grid-cols-2 grid-rows-2 gap-x-4 gap-y-5">
+            <div class="flex gap-3 p-4 item-center">
+                <img src="<?php echo get_template_directory_uri() ?>/assets/icons/check.png" alt="check icon" class="h-5">
+                <p>Respect personal space and boundaries</p>
+            </div>
+            <div class="flex gap-3 p-4 item-center">
+                <img src="<?php echo get_template_directory_uri() ?>/assets/icons/check.png" alt="check icon" class="h-5">
+                <p>Report suspicious activity immediately</p>
+            </div>
+            <div class="flex gap-3 p-4 item-center">
+                <img src="<?php echo get_template_directory_uri() ?>/assets/icons/check.png" alt="check icon" class="h-5">
+                <p>Communicate clearly through the app</p>
+            </div>
+            <div class="flex gap-3 p-4 item-center">
+                <img src="<?php echo get_template_directory_uri() ?>/assets/icons/check.png" alt="check icon" class="h-5">
+                <p>Follow local traffic laws and safety rules</p>
+            </div>
+        </div>
+    </section>
+
+    <section class="bg-slate-900">
+        <div class="wrapper py-12 flex justify-between">
+            <div class="flex flex-col justify-center">
+                <h2 class="mb-2 text-white text-2xl font-bold">Need assistance or clarifications?</h2>
+                <p class="text-white">Our safety response team is always ready to help.</p>
+            </div>
+            <div class="flex gap-4">
+                <div class="bg-white px-6 py-3.25 rounded-3xl flex gap-2 items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        class="w-5 h-5">
+                        <path stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M3 5a2 2 0 012-2h2.28a1 1 0 01.95.68l1.2 3.6a1 1 0 01-.27 1.05L8.09 9.91a16 16 0 006 6l1.58-1.07a1 1 0 011.05-.27l3.6 1.2a1 1 0 01.68.95V19a2 2 0 01-2 2h-1C9.16 21 3 14.84 3 7V5z" />
+                    </svg>
+                    <span>Contact Support</span>
+                </div>
+                <div class="rounded-3xl border-2 border-white text-white py-3 px-6 flex items-center">
+                    <p>Live Chat</p>
+                </div>
             </div>
         </div>
     </section>
